095a092023456fa5c72df8e0686a11e49153d18d
lozatona <lozatona@gmail.com>
2021-10-13 16:34:23 +0200

    Delete OneChannelActivation directory

--- OneChannelActivation/main.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-from time import sleep
-import RTDSettings as RTD
-
-RTD.GPIO_Init()
-
-while True:
-    print(RTD.RTD_ResistanceConvertion(RTD.read_ADC()))
-    sleep(10.0)
