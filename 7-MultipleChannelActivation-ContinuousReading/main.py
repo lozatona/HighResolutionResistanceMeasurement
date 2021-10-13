b54614523aff6ec6bce4fb1d7a4898bd29b164ce
lozatona <lozatona@gmail.com>
2021-10-13 16:24:38 +0200

    Update main.py

--- 7-MultipleChannelActivation-ContinuousReading/main.py
+++ 7-MultipleChannelActivation-ContinuousReading/main.py
@@ -2,17 +2,17 @@ from machine import Pin
 from time import sleep
 import RTDSettings as RTD
 
-RTD.GPIO_Init()
+RTD.GPIO_Init()                 #Initializes the channels  
 sleep(5.0)
 print("Finish RTD.GPIO_Init()")   
 while True:
-    RTD.ADC_Init()
+    RTD.ADC_Init() #Initializes the ADC
     sleep(1.0)
     x=0
     for x in range(5):
         print("RTD no. ", x)
-        RTD.Sel_Channel(x)
+        RTD.Sel_Channel(x)  #sequences the channel and current activation
         sleep(1.0)
-        print(RTD.Resistance_Convertion(RTD.Read_ADC()))
+        print(RTD.Resistance_Convertion(RTD.Read_ADC())) #begins ADC conversion and its convention into resistance value
     print("Finish ADC x 5 Channels Reading")    
     sleep(5.0)
